5fdbe76d6864755132116c0da10878427499a818
victorbrambilla <victor-brambilla@hotmail.com>
2024-09-12 18:54:31 -0400

    feat: add url entity

---
+++ src/url/url.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from 'src/user/user.entity';
+
+@Entity()
+export class Url {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  originalUrl: string;
+
+  @Column({ unique: true })
+  shortUrl: string;
+
+  @Column({ default: 0 })
+  clicks: number;
+
+  @ManyToOne(() => User, (user) => user.id, { nullable: true })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ nullable: true })
+  deletedAt: Date;   
+}
