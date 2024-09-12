8397913e70695c2f9c13a97457e51f0825cd5b16
victorbrambilla <victor-brambilla@hotmail.com>
2024-09-12 18:48:41 -0400

    feat: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, //apenas desenv
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
