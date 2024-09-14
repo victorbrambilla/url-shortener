b38e427271818d34f1140f8b994170d03d4bd95b
victorbrambilla <victor-brambilla@hotmail.com>
2024-09-14 16:44:17 -0400

    feat: add seeder for default tenant

---
+++ src/tenant/tenant-seeder.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Tenant } from './tenant.entity';
+
+@Injectable()
+export class TenantSeederService implements OnModuleInit {
+  private readonly logger = new Logger(TenantSeederService.name);
+
+  constructor(
+    @InjectRepository(Tenant)
+    private tenantRepository: Repository<Tenant>,
+  ) {}
+
+  async onModuleInit() {
+    await this.seedTenant();
+  }
+
+  private async seedTenant() {
+    const tenantExists = await this.tenantRepository.findOne({ where: { domain: 'localhost' } });
+    if (!tenantExists) {
+      const tenant = this.tenantRepository.create({
+        name: 'localhost',
+        domain: 'localhost', 
+      });
+
+      await this.tenantRepository.save(tenant);
+      this.logger.log('Default tenant seeded successfully.');
+    } else {
+      this.logger.log('Default tenant already exists.');
+    }
+  }
+}
--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { Tenant } from './tenant.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TenantService } from './tenant.service';
+import { TenantSeederService } from './tenant-seeder.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Tenant])],
     controllers: [],
-    providers: [TenantService],
+    providers: [TenantService,TenantSeederService],
     exports: [TenantService],
 })
 export class TenantModule {}
